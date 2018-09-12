e5652dbd431f534d6bdfac21eed755dd6de14fc4
Jonathan S. Fisher <exabrial@gmail.com>
2018-09-11 22:56:39 -0500

    remove annotation

--- src/main/java/com/emoneyusa/junit5/injectmap/InjectionMap.java
+++ src/main/java/com/emoneyusa/junit5/injectmap/InjectionMap.java
@@ -3,11 +3,9 @@ package com.emoneyusa.junit5.injectmap;
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-@Documented
 @Retention(RUNTIME)
 @Target(FIELD)
 public @interface InjectionMap {
