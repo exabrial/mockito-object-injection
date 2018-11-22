74d69316e7c336aff4c269544f553d6eba45a131
Jonathan S. Fisher <exabrial@gmail.com>
2018-11-22 10:44:38 -0600

    Add support for recursing upward into the class hierachy and setting any fields on parent objects

--- src/main/java/com/github/exabrial/junit5/injectmap/InjectMapExtension.java
+++ src/main/java/com/github/exabrial/junit5/injectmap/InjectMapExtension.java
@@ -2,8 +2,10 @@ package com.github.exabrial.junit5.injectmap;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 import org.junit.jupiter.api.extension.BeforeTestExecutionCallback;
@@ -44,7 +46,8 @@ public class InjectMapExtension implements BeforeTestExecutionCallback {
 				proxyFactory.setFilter(createMethodFilter());
 				Class<?> proxyClass = proxyFactory.createClass();
 				Object proxy = proxyClass.newInstance();
-				MethodHandler handler = createMethodHandler(injectMap, injectionTarget);
+				Map<String, List<Field>> fieldMap = createFieldMap(injectionTarget.getClass());
+				MethodHandler handler = createMethodHandler(injectMap, injectionTarget, fieldMap);
 				((Proxy) proxy).setHandler(handler);
 				injectMocksField.setAccessible(true);
 				injectMocksField.set(testInstance, proxy);
@@ -52,34 +55,45 @@ public class InjectMapExtension implements BeforeTestExecutionCallback {
 		}
 	}
 
-	private MethodHandler createMethodHandler(final Map<String, Object> handlerInjectMap, final Object handlerInjectionTarget) {
-		return new MethodHandler() {
-			@Override
-			public Object invoke(Object proxy, Method invokedMethod, Method proceedMethod, Object[] args) throws Throwable {
-				for (String fieldName : handlerInjectMap.keySet()) {
-					Field field = handlerInjectionTarget.getClass().getDeclaredField(fieldName);
+	private Map<String, List<Field>> createFieldMap(Class<? extends Object> targetClass) {
+		if (targetClass == Object.class) {
+			return new HashMap<>();
+		} else {
+			Map<String, List<Field>> fieldMap = createFieldMap(targetClass.getSuperclass());
+			for (Field field : targetClass.getDeclaredFields()) {
+				List<Field> fieldList = fieldMap.get(field.getName());
+				if (fieldList == null) {
+					fieldList = new LinkedList<>();
+					fieldMap.put(field.getName(), fieldList);
+				}
+				fieldList.add(field);
+			}
+			return fieldMap;
+		}
+	}
+
+	private MethodHandler createMethodHandler(final Map<String, Object> handlerInjectMap, final Object handlerInjectionTarget,
+			final Map<String, List<Field>> fieldMap) {
+		return (proxy, invokedMethod, proceedMethod, args) -> {
+			for (String fieldName : handlerInjectMap.keySet()) {
+				for (Field field : fieldMap.get(fieldName)) {
 					field.setAccessible(true);
 					field.set(handlerInjectionTarget, handlerInjectMap.get(fieldName));
 				}
-				try {
-					return invokedMethod.invoke(handlerInjectionTarget, args);
-				} catch (InvocationTargetException itEx) {
-					if (null != itEx.getCause()) {
-						throw itEx.getCause();
-					} else {
-						throw itEx;
-					}
+			}
+			try {
+				return invokedMethod.invoke(handlerInjectionTarget, args);
+			} catch (InvocationTargetException itEx) {
+				if (null != itEx.getCause()) {
+					throw itEx.getCause();
+				} else {
+					throw itEx;
 				}
 			}
 		};
 	}
 
 	private MethodFilter createMethodFilter() {
-		return new MethodFilter() {
-			@Override
-			public boolean isHandled(Method method) {
-				return !Modifier.isPrivate(method.getModifiers());
-			}
-		};
+		return method -> !Modifier.isPrivate(method.getModifiers());
 	}
 }
